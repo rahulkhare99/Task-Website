c0a432e9680ad4d4736e2fbc76f7d403bd01fe47
Rahul Raj khare <rahulrajkhare@gmail.com>
2021-01-08 17:10:12 +0400

    Added basic authentication on the get user route using jsonwebtoken, and also added middleware. The user cannot access information of other user now

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ const port = process.env.PORT || 3000
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is under maintainence!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,16 +26,13 @@ app.listen(port, () => {
 })
 
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
-     
-    console.log(password);
-    console.log(hashedPassword);
-
-    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-    console.log(isMatch);
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewproject', { expiresIn: '7 days'})
+    console.log(token);
+    const data = jwt.verify(token, 'thisismynewproject')
+    console.log(data);
 }
 
 myFunction()
