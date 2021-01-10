b946bf06fc948ba88b30169c14eabf7094a014ae
Rahul Raj khare <rahulrajkhare@gmail.com>
2021-01-10 16:36:35 +0400

    Completed authentication of users, added mapping functionality of tasks to their respective users

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ const port = process.env.PORT || 3000
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is under maintainence!')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -30,10 +18,20 @@ const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewproject', { expiresIn: '7 days'})
-    console.log(token);
     const data = jwt.verify(token, 'thisismynewproject')
-    console.log(data);
 }
 
 myFunction()
 
+const Task = require('./models/tasks')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5ffae7d463a0cb0eb031b6d2')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner);
+    const user = await User.findById('5ffae79c63a0cb0eb031b6cf')
+    await user.populate('tasks').execPopulate()
+}
+
+main()
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const Task = require('../models/tasks')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -48,6 +49,12 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewproject')
@@ -56,6 +63,14 @@ userSchema.methods.generateAuthToken = async function () {
     await user.save()
 
     return token
+}
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -76,6 +91,13 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
+    next()
+})
+
+//Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
