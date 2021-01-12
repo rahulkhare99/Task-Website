9bd12e6d402060ada66c0b9f0d3e712f3c0798c2
Rahul Raj khare <rahulrajkhare@gmail.com>
2021-01-12 18:12:06 +0400

    Added validation for file uploads and added code for handling express errors

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ const taskRouter = require('./routers/task')
 const multer = require('multer')
 const upload = multer({
     dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a word document!'))
+        }
+        cb(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 app.use(express.json())
--- src/routers/user.js
+++ src/routers/user.js
@@ -80,11 +80,22 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('File type is incorrect!'))
+        }
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
